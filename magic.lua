d79b8fd3f27207427d305124cfdfc68850993b4a
JanSharp <steinmeister36@gmail.com>
2023-12-13 21:03:41 +0100

    Update magic

--- magic.lua
+++ magic.lua
@@ -1,15 +1,15 @@
 
--- seed:                    279412857
--- setup count:             2880
--- average cubed deviation: 8.276296
--- average deviation:       0.909458/s
--- max deviation:           7.030320/s
--- iterations:              2300
+-- seed:                    3138326625
+-- setup count:             13440
+-- average cubed deviation: 11.866427
+-- average deviation:       1.097595/s
+-- max deviation:           9.690044/s
+-- iterations:              2256
 
 ---cSpell:disable
 
 return {
-  extension_distance_offset = -0x2.c92134fd20076p-4, -- -0.174103934
-  rotation_osset_from_tile_center = 0x1.340b7655271d2p-12, -- 0.000293774
-  belt_speed_multiplier = 0x2.6b4a9ab34d81ap-4, -- 0.151194195
+  extension_distance_offset = -0x3.615d22255859ap-4, -- -0.211270460
+  rotation_osset_from_tile_center = 0x1.49bbd2c93b91p-16, -- 0.000019654
+  belt_speed_multiplier = 0x2.8106ca96e5f42p-4, -- 0.156500617
 }
